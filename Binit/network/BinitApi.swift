a5f8ecd8294003307215388d082702e5cc7a6be0
Nikita Nikitin <nikitin.ninise@gmail.com>
2023-10-03 06:58:59 -0400

    some api preparation

---
+++ Binit/network/BinitApi.swift
@@ -0,0 +1,12 @@
+//
+//  BinitApi.swift
+//  Binit
+//
+//  Created by Nikita on 03.10.2023.
+//
+
+import Foundation
+
+class BinitApi {
+    
+}
---
+++ Binit/repository/BinitRepository.swift
@@ -0,0 +1,18 @@
+//
+//  BinitRepository.swift
+//  Binit
+//
+//  Created by Nikita on 03.10.2023.
+//
+
+import Foundation
+
+struct BinitRepository {
+    
+    static let shared = BinitRepository()
+    
+    private init() {}
+    
+    
+    
+}
